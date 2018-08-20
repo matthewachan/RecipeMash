63a8eef94e8d7aa787614ba4f3593a8e5a6ab5c3
Matthew Chan <matthew.a.chan@gmail.com>
2018-08-20 16:19:54 -0400

    Starting to spice up the UI

--- app/static/js/App.jsx
+++ app/static/js/App.jsx
@@ -6,24 +6,29 @@ const { Header, Footer, Sider, Content } = Layout
 
 const Search = Input.Search;
 const FormItem = Form.Item;
+
 const colors = ['magenta', 'red', 'volcano', 'orange', 'gold', 'lime', 'green', 'cyan', 'blue', 'geekblue', 'purple'];
+
 function pickColor() {
-  return colors[Math.floor(Math.random() * colors.length)];
+  return 'geekblue';
 };
 
 export default class App extends React.Component {
   render() {
     return (
         <div>
-            <div><Top /></div>
-            <div style={{ padding: '15px 50px' }}><Main /></div>
+            <div ><Main /></div>
         </div>
     )
   }
 };
 
 const Top = () => (
-  <h1>I am header.</h1>
+  <Row justify='center'>
+      <Col span={10} offset={7}>
+          <h1 className='alignCenter' id='banner'><Link to={{ pathname: '/' }}>RECIPE MASH</Link></h1>
+      </Col>
+  </Row>
 );
 
 class Recipes extends React.Component {
@@ -63,22 +68,20 @@ class Recipes extends React.Component {
   render() {
     return (
       <div>
-        <Row justify='center'>
-            <Col span={24}>
-              
-            </Col>
-        </Row>
-        <Row justify='center'>
+      <div style={{ paddingTop: '35px'}}><Top /></div>
+        <Row style={{ padding: '15px 50px' }} justify='center'>
             <Col span={24}>
                 <List header={<div>Search terms: { this.getSearchTerms() }</div>} itemLayout='vertical' loading={this.loading} size='large' dataSource={this.state.recipes} pagination={{pageSize: 5}}
                 renderItem={item => (
                   <List.Item key={item.name} actions={[<span><Icon type="star" style={{ marginRight: 8 }}/>{item.rating} (XX reviews)</span>]}
-                 extra={<img width={272} src={item.image_url} />}>
+                 extra={<img width={272} style={{ display: 'inline-block' }} src={item.image_url ? item.image_url : ""} />}>
                   <List.Item.Meta avatar={<Avatar src={item.image_url} />} 
                   title={<a href={item.url}>{item.name}</a>}
-                  description={item.author}
+                  description={item.total_time}
                   />
-                        {item.ingredients.split('|').map(ingredient => (<Tag key={ingredient} color={pickColor()}>{ingredient}</Tag>))}
+                    <ul style={{columns: 2, listStylePosition: 'inside'}}>
+                        {item.ingredients.split('|').map(ingredient => (<li >{ingredient}</li>))}
+                    </ul>
                   </List.Item>
                 )}
                 />
@@ -176,7 +179,9 @@ class Home extends React.Component {
           }
         };
         return (
-            <Row justify='center'>
+            <div className='bg'>
+            <div style={{ paddingTop: '35px' }}><Top /></div>
+            <Row style={{ padding: '15px 50px' }} justify='center'>
                 <Col span={10}>
                     <h1>Main photo here</h1>
                 </Col>
@@ -189,7 +194,7 @@ class Home extends React.Component {
                             <Input className='form-control' value={this.state.input} onChange={this.onChangeInput} type='text' size='small' suffix={<Icon type="plus" />} placeholder='Add an ingredient' />
                         </FormItem>
                         <FormItem {...formItemLayout}>
-                        <List
+                        <List id='ingredient-list'
                             bordered
                             locale={{emptyText: 'No ingredients'}}
                             size='small'
@@ -198,11 +203,12 @@ class Home extends React.Component {
                         />
                         </FormItem>
                         <FormItem {...tailFormItemLayout}>
-                            <Link to={{ pathname: '/recipes', search: this.getQueryString() }}><Button disabled={this.state.disabled} type='primary'>Mash</Button></Link>
+                            <Link to={{ pathname: '/recipes', search: this.getQueryString() }}><Button size='large' disabled={this.state.disabled} type='primary'>MASH</Button></Link>
                         </FormItem>
                     </Form>
                 </Col>
           </Row>
+          </div>
         );
     }
 }
